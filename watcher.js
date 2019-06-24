2f5de6622f5fb45be3e1a99d4cb9750ab1b04f89
Adrien M <adrien@d.tube>
2019-06-24 16:24:58 +0200

    fix bug

--- watcher.js
+++ watcher.js
@@ -5,7 +5,7 @@ const creator = 'dtube'
 const prefix = ''
 const give_bw = 50000
 const give_vt = 1000
-const give_dtc = 100
+const give_dtc = 10000
 
 steem.api.streamBlock(function(err, res) {
     if (err) throw err;
@@ -21,62 +21,66 @@ steem.api.streamBlock(function(err, res) {
                     return
                 }
                 
-                if (json && json.profile && json.profile.dtube_pub) {
-                    var pubKey = json.profile.dtube_pub
-                    console.log('Creating '+username+' '+pubKey)
-                    var txData = {
-                        pub: pubKey,
-                        name: prefix+username
-                    }
-                    var newTx = {
-                        type: 0,
-                        data: txData
-                    }
-                    newTx = javalon.sign(wif, creator, newTx)
-                    javalon.sendTransaction(newTx, function(err, res) {
-                        if (err) return
-                        console.log('Feeding '+username)
-                        setTimeout(function() {
-                            if (give_vt) {
-                                var newTx = {
-                                    type: 14,
-                                    data: {
-                                        amount: give_vt,
-                                        receiver: prefix+username
-                                    }
-                                }
-                                newTx = javalon.sign(wif, creator, newTx)
-                                javalon.sendTransaction(newTx, function(err, res) {})
-                            }
-
-                            if (give_bw) {
-                                var newTx = {
-                                    type: 15,
-                                    data: {
-                                        amount: give_bw,
-                                        receiver: prefix+username
-                                    }
-                                }
-                                newTx = javalon.sign(wif, creator, newTx)
-                                javalon.sendTransaction(newTx, function(err, res) {})
-                            }
-                            
-                            if (give_dtc) {
-                                var newTx = {
-                                    type: 3,
-                                    data: {
-                                        amount: give_dtc,
-                                        receiver: prefix+username,
-                                        memo: 'Welcome to DTube chain!'
-                                    }
-                                }
-                                newTx = javalon.sign(wif, creator, newTx)
-                                javalon.sendTransaction(newTx, function(err, res) {})
-                            }
-                        }, 6000)
-                    })
-                }
+                createAccAndFeed(username, json)
             }
         }
     }
 })
+
+function createAccAndFeed(username, json) {
+    if (json && json.profile && json.profile.dtube_pub) {
+        var pubKey = json.profile.dtube_pub
+        console.log('Creating '+username+' '+pubKey)
+        var txData = {
+            pub: pubKey,
+            name: prefix+username
+        }
+        var newTx = {
+            type: 0,
+            data: txData
+        }
+        newTx = javalon.sign(wif, creator, newTx)
+        javalon.sendTransaction(newTx, function(err, res) {
+            if (err) return
+            console.log('Feeding '+username)
+            setTimeout(function() {
+                if (give_vt) {
+                    var newTx = {
+                        type: 14,
+                        data: {
+                            amount: give_vt,
+                            receiver: prefix+username
+                        }
+                    }
+                    newTx = javalon.sign(wif, creator, newTx)
+                    javalon.sendTransaction(newTx, function(err, res) {})
+                }
+
+                if (give_bw) {
+                    var newTx = {
+                        type: 15,
+                        data: {
+                            amount: give_bw,
+                            receiver: prefix+username
+                        }
+                    }
+                    newTx = javalon.sign(wif, creator, newTx)
+                    javalon.sendTransaction(newTx, function(err, res) {})
+                }
+                
+                if (give_dtc) {
+                    var newTx = {
+                        type: 3,
+                        data: {
+                            amount: give_dtc,
+                            receiver: prefix+username,
+                            memo: 'Welcome to DTube chain!'
+                        }
+                    }
+                    newTx = javalon.sign(wif, creator, newTx)
+                    javalon.sendTransaction(newTx, function(err, res) {})
+                }
+            }, 6000)
+        })
+    }
+}
